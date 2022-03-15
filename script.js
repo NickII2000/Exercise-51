bcebf2dcb787feb521b7f21adba8104916f3cfee
Nick <Nick_II2000@mail.ru>
2022-03-15 23:38:43 +0600

    7-th commit

--- script.js
+++ script.js
@@ -14,5 +14,6 @@ const clone = JSON.parse(JSON.stringify(person));
 clone.parents.mom = 'Ann';
 console.log(JSON.stringify(person));
 console.log(person);
+console.log('======================');
 console.log(JSON.stringify(clone));
 console.log(clone);
