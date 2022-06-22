1df2ce1df925a52ae45b5d0794b0a66e0d85ec99
Nick <Nick_II2000@mail.ru>
2022-06-22 06:59:09 +0600

    8-th commit

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 const person = {
     name: 'Alex',
     tel: '+79134445566',
-    age: 25,
-    parents: {
-        mom: 'Olga',
-        dad: 'Mike',
-        married: 20,
-    },
+    // age: 25,
+    // parents: {
+    //     mom: 'Olga',
+    //     dad: 'Mike',
+    //     married: 20,
+    // },
 };
 
-const clone = JSON.parse(JSON.stringify(person));
-clone.parents.mom = 'Ann';
+// const clone = JSON.parse(JSON.stringify(person));
+// clone.parents.mom = 'Ann';
 console.log(JSON.stringify(person));
-console.log(person);
-console.log('======================');
-console.log(JSON.stringify(clone));
-console.log(clone);
+// console.log(person);
+// console.log('======================');
+// console.log(JSON.stringify(clone));
+// console.log(clone);
